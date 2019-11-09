7f59ac76384cd480319a48092cac1c19943b8dba
Serguei Khasimkhanov <serkhas@hotmail.com>
2019-11-09 04:22:44 -0500

    organizing constants.js

--- ww/static-content-react/lib/constants.js
+++ ww/static-content-react/lib/constants.js
@@ -1,5 +1,7 @@
-global.wwPort = 10600; // CHANGE ME
-global.wwWsPort = global.wwPort+1; // for websockets DON'T CHANGE
-// global.wwHostname = "cslinux.utm.utoronto.ca"; DON'T CHANGE
-global.wwHostname = "localhost"; // CHANGE ME
-global.wwWsURL = "ws://"+global.wwHostname+":"+global.wwWsPort; // DON'T CHANGE
+// Can change:
+global.wwPort = 10600;
+global.wwHostname = "localhost"; // Change if running anywhere but localhost
+
+// Don't change:
+global.wwWsPort = global.wwPort+1;
+global.wwWsURL = "ws://"+global.wwHostname+":"+global.wwWsPort;
